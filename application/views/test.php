b90a1f22cf03e328478abbf516bd14019756024f
Emre Can Yılmaz <e.canyilmaz@gmail.com>
2015-08-20 21:59:39 +0300

    login page

---
+++ application/views/test.php
@@ -0,0 +1,91 @@
+<html>
+<head>
+	<title></title>
+	<script type="text/javascript" src="script/jquery-1.11.3.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="semantic/dist/semantic.min.css">
+	<script src="semantic/dist/semantic.min.js"></script>
+
+</head>
+<body>
+
+	<div class="ui attached stackable menu">
+	  <div class="ui container">
+	    <a class="item">
+	      <i class="home icon"></i> Home
+	    </a>
+	    <a class="item">
+	      <i class="grid layout icon"></i> Browse
+	    </a>
+	    <a class="item">
+	      <i class="mail icon"></i> Messages
+	    </a>
+	    <div class="ui simple dropdown item">
+	      More
+	      <i class="dropdown icon"></i>
+	      <div class="menu">
+	        <a class="item"><i class="edit icon"></i> Edit Profile</a>
+	        <a class="item"><i class="globe icon"></i> Choose Language</a>
+	        <a class="item"><i class="settings icon"></i> Account Settings</a>
+	      </div>
+	    </div>
+	    <div class="right item">
+	      <div class="ui input"><input type="text" placeholder="Search..."></div>
+	    </div>
+	  </div>
+	</div>
+
+
+	<div class="ui four column doubling grid container">
+	  <div class="column">
+	    <div class="ui card">
+		  <div class="image">
+		    <img src="http://semantic-ui.com/images/avatar2/large/kristy.png">
+		  </div>
+		  <div class="content">
+		    <a class="header">Kristy</a>
+		    <div class="meta">
+		      <span class="date">Joined in 2013</span>
+		    </div>
+		    <div class="description">
+		      Kristy is an art director living in New York.
+		    </div>
+		  </div>
+		  <div class="extra content">
+		    <a>
+		      <i class="user icon"></i>
+		      22 Friends
+		    </a>
+		  </div>
+		</div>
+	  </div>
+	  <div class="column">
+	    <button class="ui red basic button">Red</button>
+		<button class="ui orange basic button">Orange</button>
+		<button class="ui yellow basic button">Yellow</button>
+		<button class="ui olive basic button">Olive</button>
+		<button class="ui green basic button">Green</button>
+		<button class="ui teal basic button">Teal</button>
+		<button class="ui blue basic button">Blue</button>
+		<button class="ui violet basic button">Violet</button>
+		<button class="ui purple basic button">Purple</button>
+		<button class="ui pink basic button">Pink</button>
+		<button class="ui brown basic button">Brown</button>
+		<button class="ui grey basic button">Grey</button>
+		<button class="ui black basic button">Black</button>
+	  </div>
+	  <div class="column">
+	    <button class="ui basic button">
+		  <i class="icon user"></i>
+		  Add Friend
+		</button>
+	    <p>sfsdfsdf</p>
+	  </div>
+	  <div class="column">
+	    <p>sdf</p>
+	    <p>fsdkfsodfksodf</p>
+	  </div>
+	</div>
+
+	
+</body>
+</html>
--- application/views/welcome.php
+++ application/views/welcome.php
@@ -4,85 +4,50 @@
 	<script type="text/javascript" src="script/jquery-1.11.3.min.js"></script>
 	<link rel="stylesheet" type="text/css" href="semantic/dist/semantic.min.css">
 	<script src="semantic/dist/semantic.min.js"></script>
-
 </head>
 <body>
 
 	<div class="ui attached stackable menu">
 	  <div class="ui container">
 	    <a class="item">
-	      <i class="home icon"></i> Home
+	   		LOGO
 	    </a>
-	    <a class="item">
-	      <i class="grid layout icon"></i> Browse
-	    </a>
-	    <a class="item">
-	      <i class="mail icon"></i> Messages
-	    </a>
-	    <div class="ui simple dropdown item">
-	      More
-	      <i class="dropdown icon"></i>
-	      <div class="menu">
-	        <a class="item"><i class="edit icon"></i> Edit Profile</a>
-	        <a class="item"><i class="globe icon"></i> Choose Language</a>
-	        <a class="item"><i class="settings icon"></i> Account Settings</a>
-	      </div>
-	    </div>
-	    <div class="right item">
-	      <div class="ui input"><input type="text" placeholder="Search..."></div>
-	    </div>
 	  </div>
 	</div>
 
 
-	<div class="ui four column doubling grid container">
-	  <div class="column">
-	    <div class="ui card">
-		  <div class="image">
-		    <img src="http://semantic-ui.com/images/avatar2/large/kristy.png">
-		  </div>
-		  <div class="content">
-		    <a class="header">Kristy</a>
-		    <div class="meta">
-		      <span class="date">Joined in 2013</span>
+	<div class="ui doubling grid container">
+	  <div class="column eight wide centered" style="margin-top:100px;">
+	  	<div class="ui two column middle aligned very relaxed stackable grid">
+		  <div class="column">
+		    <div class="ui form">
+		      <div class="field">
+		        <label>Username</label>
+		        <div class="ui left icon input">
+		          <input type="text" placeholder="Username">
+		          <i class="user icon"></i>
+		        </div>
+		      </div>
+		      <div class="field">
+		        <label>Password</label>
+		        <div class="ui left icon input">
+		          <input type="password">
+		          <i class="lock icon"></i>
+		        </div>
+		      </div>
+		      <div class="ui blue submit button">Login</div>
 		    </div>
-		    <div class="description">
-		      Kristy is an art director living in New York.
-		    </div>
 		  </div>
-		  <div class="extra content">
-		    <a>
-		      <i class="user icon"></i>
-		      22 Friends
-		    </a>
+		  <div class="ui vertical divider">
+		    Or
+		  </div>
+		  <div class="center aligned column">
+		    <div class="ui big green labeled icon button">
+		      <i class="signup icon"></i>
+		      Sign Up
+		    </div>
 		  </div>
 		</div>
-	  </div>
-	  <div class="column">
-	    <button class="ui red basic button">Red</button>
-		<button class="ui orange basic button">Orange</button>
-		<button class="ui yellow basic button">Yellow</button>
-		<button class="ui olive basic button">Olive</button>
-		<button class="ui green basic button">Green</button>
-		<button class="ui teal basic button">Teal</button>
-		<button class="ui blue basic button">Blue</button>
-		<button class="ui violet basic button">Violet</button>
-		<button class="ui purple basic button">Purple</button>
-		<button class="ui pink basic button">Pink</button>
-		<button class="ui brown basic button">Brown</button>
-		<button class="ui grey basic button">Grey</button>
-		<button class="ui black basic button">Black</button>
-	  </div>
-	  <div class="column">
-	    <button class="ui basic button">
-		  <i class="icon user"></i>
-		  Add Friend
-		</button>
-	    <p>sfsdfsdf</p>
-	  </div>
-	  <div class="column">
-	    <p>sdf</p>
-	    <p>fsdkfsodfksodf</p>
 	  </div>
 	</div>
 
